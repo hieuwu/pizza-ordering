7c33992cc9a84afc499475bc4de97eb6626bf749
hieuwu <43868345+hieuwu@users.noreply.github.com>
2020-04-22 20:14:30 +0700

    refactor(resource): define resource files
    Add string, dimension, color files

---
+++ PycozzaProject/app/resources/colors.js
@@ -0,0 +1,8 @@
+const color = {
+    mainColor: '#e4283d',
+    activeColor: '#0F4C81',
+    inactiveStatus: '#bbbdbf',
+    textColor: '#ffffff',
+    promptText: '#474747'
+};
+export default color;
---
+++ PycozzaProject/app/resources/strings.js
@@ -0,0 +1,15 @@
+const string = {
+    promptFirstName: 'First name',
+    promptLastName: 'Last name',
+    promptPhone: 'Phone number',
+    promptEmail: 'Email',
+    buttonSignIn: 'Sign in',
+    buttonSignUp: 'Sign up',
+    buttonCreateAccount: 'Create account',
+    buttonAddToCart: 'Add to cart',
+    promptPassword:' Password',
+    promptConfirmPass: 'Confirm password',
+    promptExistedAccount: 'Already have an account?'
+}
+
+export default string;

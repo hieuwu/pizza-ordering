5c5abd5b574cac6b71bab5125b03575768a0f27c
hieuwu <43868345+hieuwu@users.noreply.github.com>
2020-04-23 14:26:29 +0700

    refactor(ovalshape): create a oval shape component
    Add oval shape to product list screen

---
+++ PycozzaProject/app/components/OvalShape.component.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import {View, StyleSheet } from 'react-native'
+import dimension from '../resources/dimensions';
+import color from '../resources/colors'
+export default class OvalShape extends Component {
+    render() {
+        return (
+            <View style={styles.ovalStyle}>
+            </View>
+        )
+    }
+}
+const styles = StyleSheet.create({
+    ovalStyle: {
+        width:dimension.window.height+ 100,
+        height: dimension.window.height,
+        backgroundColor: color.headerBackground,
+        position: 'absolute',
+        borderRadius: 400,
+        top: -dimension.window.height/2 - 230,
+    }
+})
+
+

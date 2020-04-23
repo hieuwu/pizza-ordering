dc4f3bbf85c5971bdf7b94e826e7e4ad20d29a46
hieuwu <43868345+hieuwu@users.noreply.github.com>
2020-04-23 14:23:53 +0700

    refactor(headerstyle): add style for header
    Add style for title, background for header bar

---
+++ PycozzaProject/app/navigation/Header.style.js
@@ -0,0 +1,14 @@
+import color from '../resources/colors'
+const headerStyle = {
+    headerStyle: {
+        backgroundColor: color.headerBackground,
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+        fontWeight: 'bold',
+    },
+    headerTitleAlign: 'center',
+    
+}
+
+export default headerStyle;

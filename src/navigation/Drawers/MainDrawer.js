bb4997dfe13fa469b809b95e14b2d3c5649bb3b4
hoangnamu <hoangnamle1998@gmail.com>
2020-04-27 22:22:52 +0700

    refactor(pizzaList): refactor MainDrawer and CategoriesStack

--- src/navigation/Drawers/MainDrawer.js
+++ src/navigation/Drawers/MainDrawer.js
@@ -5,10 +5,11 @@ import colors from '../../modules/resources/colors/Colors';
 
 import CategoriesStack from '../Stacks/CategoriesStack';
 import CartStack from '../Stacks/CartStack';
+import PizzaMenuStack from '../Stacks/PizzaMenuStack';
 
 const Drawer = createDrawerNavigator();
 
-export default class MainStack extends Component {
+export default class MainDrawer extends Component {
   render() {
     return (
       <Drawer.Navigator
@@ -26,6 +27,7 @@ export default class MainStack extends Component {
           },
         }}>
         <Drawer.Screen name="Home" component={CategoriesStack} />
+        <Drawer.Screen name="PizzaMenu" component={PizzaMenuStack} />
         <Drawer.Screen name="Cart" component={CartStack} />
       </Drawer.Navigator>
     );
--- src/navigation/Stacks/CategoriesStack.js
+++ src/navigation/Stacks/CategoriesStack.js
@@ -8,7 +8,7 @@ import PizzaMenu from '../../modules/view/PizzaMenu/PizzaMenu.screen';
 
 const Stack = createStackNavigator();
 
-export default class MenuStack extends Component {
+export default class CategoriesStack extends Component {
   render() {
     return (
       <Stack.Navigator initialRouteName="categories">

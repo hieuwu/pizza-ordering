27678e871a46037484c4d3f563f3b1d8acde8453
hieuwu <43868345+hieuwu@users.noreply.github.com>
2020-04-29 18:41:09 +0700

    refactor(resource): add color string
    Add new color and string and dimension

--- PycozzaProject/app/resources/strings.js
+++ PycozzaProject/app/resources/strings.js
@@ -7,16 +7,21 @@ const string = {
     buttonSignUp: 'Sign up',
     buttonCreateAccount: 'Create account',
     buttonAddToCart: 'Add to cart',
+    buttonCheckOut: 'Check out',
     promptPassword:' Password',
+    propmptEmptyCart: 'Your cart is empty. Buy something !',
     promptConfirmPass: 'Confirm password',
     promptExistedAccount: 'Already have an account?',
+    promptItemCost: 'Items / Total cost:',
     listScreenTitle: 'PIZZA',
     categories: {
         pizza: 'Pizza',
         sidedish: 'Side dish',
         dessert: 'Desert',
         beverage: 'Bevergae',
-    }
+    },
+    promptTotal: 'Total: ',
+    currency: 'VNĐ'
 }
 
 export default string;

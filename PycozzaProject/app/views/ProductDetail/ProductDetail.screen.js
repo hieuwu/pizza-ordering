28c4e5554e65b5c9811242ae9c8c7e1c80b01357
hieuwu <43868345+hieuwu@users.noreply.github.com>
2020-05-01 22:22:49 +0700

    refactor(productdetail): fix add item
    Fix add item to cart with redux

--- PycozzaProject/app/views/ProductDetail/ProductDetail.screen.js
+++ PycozzaProject/app/views/ProductDetail/ProductDetail.screen.js
@@ -13,7 +13,10 @@ import store from '../../redux/store';
 import { connect } from 'react-redux';
 import { addToCart } from '../../redux/actions/index';
 
-let i = 1
+let i = 1;
+let id = 0;
+let cartID = 0;
+
 class ProductDetailScreen extends Component {
     constructor(props) {
         super(props);
@@ -28,36 +31,56 @@ class ProductDetailScreen extends Component {
     mediumSizeChecked = () => {
         this.setState({ sizeChecked: true });
         this.setState({ size: 'M' });
-        console.log("M")
     }
     largeSizeChecked = () => {
         this.setState({ sizeChecked: false });
         this.setState({ size: 'L' });
-        console.log("L");
     }
     thinCrustChecked = () => {
         this.setState({ crustChecked: true });
         this.setState({ crust: 'Thin' });
-        console.log("Thin crust");
     }
     thickCrustChecked = () => {
         this.setState({ crustChecked: false });
         this.setState({ crust: 'Thick' });
-        console.log("Think crust");
     }
+    getParam =() => {
+        const {item} = this.props.route.params;
+        let param = {};
+        param.id= JSON.stringify(item.id).replace(/['"]+/g, '');
+        param.name= JSON.stringify(item.name).replace(/['"]+/g, '') ;
+        param.categoryId= JSON.stringify(item.categoryId).replace(/['"]+/g, '');
+        param.imgLink= JSON.stringify(item.imgLink).replace(/['"]+/g, '');
+        param.price= JSON.stringify(item.price).replace(/['"]+/g, '') ;
+        param.maxPrice= JSON.stringify(item.maxPrice).replace(/['"]+/g, '');
+        param.description= JSON.stringify(item.description).replace(/['"]+/g, '');
+        return param;
 
-    createOrderLine = () => {
-        console.log('1');
+    }
+      createOrderLine = () => {
         const {addToCart} = this.props;
+        const item = this.getParam();
         let orderLine = {};
         orderLine.type = 'ADD_TODO'
-        orderLine.id = i++;
-        orderLine.text = 'Test';
+        orderLine.id =   String(cartID);
+        orderLine.name = item.name;
+        if (this.state.size === 'M')
+        {
+            orderLine.price = item.price;
+        } else {
+            orderLine.price = item.maxPrice;
+        }
+        orderLine.quantity= this.state.quantity;
+        orderLine.size= this.state.size;
+        orderLine.crust= this.state.crust;
+        orderLine.imgLink= item.imgLink;
         addToCart(orderLine);
+        console.log('Cart ID: ',cartID);
+        cartID = cartID + 1;
     }
 
     render() {
-        const { item } = this.props.route.params;
+        const item  = this.getParam();
         return (
             <View style={styles.container}>
                 <ScrollView>
@@ -65,13 +88,13 @@ class ProductDetailScreen extends Component {
                         <OvalShape />
                     </View>
                     <View style={{ alignItems: 'center' }}>
-                        <Image style={{ width: 200, height: 200, borderRadius: 200 / 2, resizeMode: 'contain' }} source={{ uri: JSON.stringify(item.imageURL).replace(/['"]+/g, '') }} />
-                        <Text style={styles.productTitle}>{JSON.stringify(item.name).replace(/['"]+/g, '')}</Text>
-                        <Text style={styles.priceTitle}>{JSON.stringify(item.price).replace(/['"]+/g, '')} VNĐ</Text>
+                        <Image style={{ width: 200, height: 200, borderRadius: 200 / 2, resizeMode: 'contain' }} source={{ uri:item.imgLink}} />
+                        <Text style={styles.productTitle}>{item.name}</Text>
+                        <Text style={styles.priceTitle}>{item.price + ',000'} - {item.maxPrice+ ',000'} VNĐ</Text>
                     </View>
                     <View style={{ marginHorizontal: 15, }}>
                         <Text style={styles.subTitle}>Description:</Text>
-                        <Text style={styles.content}>A large disc of dough, covered with tomato paste, then either only grated cheese or pieces of mozzarella cheese, or other toppings like chopped vegetables, sausages, salami, </Text>
+                        <Text style={styles.content}>{item.description} </Text>
                     </View>
                     <View style={{ marginHorizontal: 15, }}>
                         <Text style={[styles.subTitle, { marginTop: 15 }]}>Size:</Text>
@@ -131,7 +154,7 @@ class ProductDetailScreen extends Component {
                         </View>
                     </View>
                 </ScrollView>
-                <Button onPress={
+                <Button onPress={ 
                     this.createOrderLine} title={string.buttonAddToCart} buttonStyle={styles.addCart} />
             </View>
         )

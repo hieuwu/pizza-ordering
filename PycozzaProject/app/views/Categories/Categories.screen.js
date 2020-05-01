a79f5529986d9d422d57eb26e547a9cbb75ead25
hieuwu <43868345+hieuwu@users.noreply.github.com>
2020-05-01 22:20:46 +0700

    refactor(categories): add navigation
    Add navigation from categories to list screens

--- PycozzaProject/app/views/Categories/Categories.screen.js
+++ PycozzaProject/app/views/Categories/Categories.screen.js
@@ -7,28 +7,28 @@ export default class CategoriesScreen extends Component {
     render() {
         return (
             <ScrollView style={styles.container}>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => this.props.navigation.navigate('ProductList', {title: 'Pizza'})}>
                     <ImageBackground style={styles.imageStyle} source={require('../../resources/images/category-pizza.jpg')}>
                         <Text style={styles.title}>
                             {string.categories.pizza}
                         </Text>
                     </ImageBackground>
-                    </TouchableOpacity>
-                    <TouchableOpacity>
+                    </TouchableOpacity  >
+                    <TouchableOpacity onPress={() => this.props.navigation.navigate('ProductList', {title: 'Side dish'})}>
                     <ImageBackground style={styles.imageStyle} source={require('../../resources/images/category-sidedish.jpg')}>
                         <Text style={styles.title}>
                             {string.categories.sidedish}
                         </Text>
                     </ImageBackground>
                     </TouchableOpacity>
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={() => this.props.navigation.navigate('ProductList', {title: 'Desert'})}>
                     <ImageBackground style={styles.imageStyle} source={require('../../resources/images/category-dessert.jpg')}>
                         <Text style={styles.title}>
                             {string.categories.dessert}
                         </Text>
                     </ImageBackground>
-                    </TouchableOpacity>
-                    <TouchableOpacity>
+                    </TouchableOpacity >
+                    <TouchableOpacity onPress={() => this.props.navigation.navigate('ProductList', {title: 'Beverage'})}>
                     <ImageBackground style={styles.imageStyle} source={require('../../resources/images/category-beverage.jpg')}>
                         <Text style={styles.title}>
                             {string.categories.beverage}

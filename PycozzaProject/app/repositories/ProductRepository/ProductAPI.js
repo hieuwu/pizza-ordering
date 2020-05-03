cf81c3ef179177573400ac66f034a4d8dee6133d
hieuwu <43868345+hieuwu@users.noreply.github.com>
2020-05-03 21:42:38 +0700

    refactor(productrepo): fetch data for each category
    Add fetch data for each category

--- PycozzaProject/app/repositories/ProductRepository/ProductAPI.js
+++ PycozzaProject/app/repositories/ProductRepository/ProductAPI.js
@@ -1,9 +1,26 @@
 import BaseAPI from '../../services/BaseAPI'
 
 class ProductAPI {
-    async getListProduct(){
-        let data = await BaseAPI.get('/product/1', );
-        return data;
+    async getListProduct(type) {
+        let data = [];
+        switch (type) {
+            case 'pizza': {
+                data = await BaseAPI.get('/product/1');
+                return data;
+            };
+            case 'sidedish': {
+                data = await BaseAPI.get('/product/2');
+                return data;
+            };
+            case 'dessert': {
+                data = await BaseAPI.get('/product/3');
+                return data;
+            };
+            case 'beverage': {
+                data = await BaseAPI.get('/product/4');
+                return data;
+            };
+        }
     }
 }
 
--- PycozzaProject/app/repositories/ProductRepository/ProductRepo.js
+++ PycozzaProject/app/repositories/ProductRepository/ProductRepo.js
@@ -1,10 +1,10 @@
 import ProductDAO from './ProductDAO'
 import ProductAPI from './ProductAPI';
 class ProductRepo {
-    async getListProduct(){
+    async getListProduct(type){
         // if DAO is empty, fetch data from API
         console.log("Get from REPO...");
-        let a = await new ProductAPI().getListProduct();
+        let a = await new ProductAPI().getListProduct(type);
         return a;
     }
 }

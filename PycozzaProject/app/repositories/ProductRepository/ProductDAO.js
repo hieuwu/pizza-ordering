5894e64491fe32d9b80637cc3ab671ec6fe4feb5
hieuwu <43868345+hieuwu@users.noreply.github.com>
2020-05-05 09:55:37 +0700

    refactor(productdao): add saving list to local
    Add saving list product to local foreach one

--- PycozzaProject/app/repositories/ProductRepository/ProductDAO.js
+++ PycozzaProject/app/repositories/ProductRepository/ProductDAO.js
@@ -1,8 +1,107 @@
+import AsyncStorage from '@react-native-community/async-storage';
+
 class ProductDAO {
-    getListProduct(){
+    async getListProduct(type){
         console.log("Get from DAO...");
-        let data = productList;
-        return data;
+        let data = [];
+        switch(type) {
+            case 'pizza':
+                {
+                    console.log("Get data from", type);
+                    try {
+                        data = await AsyncStorage.getItem('pizza') || 'none';
+                        if (data === 'none')
+                        {
+                            return 'none';
+                        }
+                    } catch (error) {
+                        console.log(error.message);
+                    }
+                    return JSON.parse(data);
+                }
+            case 'sidedish':
+                {
+                    console.log("Get data from", type);
+                    try {
+                        data = await AsyncStorage.getItem('sidedish') || 'none';
+                        if (data === 'none')
+                        {
+                            return 'none';
+                        }
+                    } catch (error) {
+                        console.log(error.message);
+                    }
+                    return JSON.parse(data);
+                }
+            case 'dessert':
+                {
+                    console.log("Get data from", type);
+                    try {
+                        data = await AsyncStorage.getItem('dessert') || 'none';
+                        if (data === 'none')
+                        {
+                            return 'none';
+                        }
+                    } catch (error) {
+                        console.log(error.message);
+                    }
+                    return JSON.parse(data);
+                }
+            case 'beverage':
+                {
+                    console.log("Get data from", type);
+                    try {
+                        data = await AsyncStorage.getItem('beverage') || 'none';
+                        if (data === 'none')
+                        {
+                            return 'none';
+                        }
+                    } catch (error) {
+                        console.log(error.message);
+                    }
+                    return JSON.parse(data);
+                }
+        }
+
+    }
+    async saveListProduct(type, productList) {
+        console.log('Saved to local');
+        let productListJSON = JSON.stringify(productList);
+        switch(type) {
+            case 'pizza': {
+                try {
+                    await AsyncStorage.setItem('pizza', productListJSON);
+                } catch (error) {
+                    console.log(error.message);
+                }
+                break;
+            }
+            case 'sidedish': {
+                try {
+                    await AsyncStorage.setItem('sidedish', productListJSON);
+                } catch (error) {
+                    console.log(error.message);
+                }
+                break;
+            }
+            case 'dessert': {
+                try {
+                    await AsyncStorage.setItem('dessert', productListJSON);
+                } catch (error) {
+                    console.log(error.message);
+                }
+                break;
+            }
+            case 'beverage': {
+                try {
+                    await AsyncStorage.setItem('beverage', productListJSON);
+                } catch (error) {
+                    console.log(error.message);
+                }
+                break;
+            }
+        }
+
     }
 }
 

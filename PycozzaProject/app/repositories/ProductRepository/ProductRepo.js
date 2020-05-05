992476241c7c2cefea0ce2b2ac2318c392c8cfed
hieuwu <43868345+hieuwu@users.noreply.github.com>
2020-05-05 09:57:53 +0700

    refactor(productrepo): handle get data
    Handle get data from local and api

--- PycozzaProject/app/repositories/ProductRepository/ProductRepo.js
+++ PycozzaProject/app/repositories/ProductRepository/ProductRepo.js
@@ -2,10 +2,16 @@ import ProductDAO from './ProductDAO'
 import ProductAPI from './ProductAPI';
 class ProductRepo {
     async getListProduct(type){
-        // if DAO is empty, fetch data from API
-        console.log("Get from REPO...");
-        let a = await new ProductAPI().getListProduct(type);
-        return a;
+        let data = await new ProductDAO().getListProduct(type);
+        if (data == 'none') {
+            data = await new ProductAPI().getListProduct(type);
+            return data;
+        }
+        return data;
+    }
+
+    async saveListProduct(type,productList) {
+        await new ProductDAO().saveListProduct(type,productList)
     }
 }
 

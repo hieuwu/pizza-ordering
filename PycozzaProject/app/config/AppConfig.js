9be5377023f3f53d113c179134154d9470aa0832
hieuvu99 <star.hieu.99@gmail.com>
2020-05-07 14:37:17 +0700

    refactor(config): change baseurl
    Add change baseurl

--- PycozzaProject/app/config/AppConfig.js
+++ PycozzaProject/app/config/AppConfig.js
@@ -1,6 +1,6 @@
 const AppConfig = {
     API: {
-        baseURL: 'http://192.168.1.11:8080/pycozza'
+        baseURL: 'http://192.168.1.5:8080/pycozza'
 },
     IMAGE: {
         baseURL: 'https://drive.google.com/uc?export=view&id=',

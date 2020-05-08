437591c960028cceb0250139a20aec6de4b9962e
hieuvu99 <star.hieu.99@gmail.com>
2020-05-08 07:41:05 +0700

    refactor(categoryscreen): change type of product
    Change type of product follow api

--- PycozzaProject/app/views/Categories/Categories.screen.js
+++ PycozzaProject/app/views/Categories/Categories.screen.js
@@ -7,28 +7,28 @@ export default class CategoriesScreen extends Component {
     render() {
         return (
             <ScrollView style={styles.container}>
-                <TouchableOpacity onPress={() => this.props.navigation.navigate('ProductList', {title: string.categories.pizza, type:'pizza'})}>
+                <TouchableOpacity onPress={() => this.props.navigation.navigate('ProductList', {title: string.categories.pizza, type:'1'})}>
                     <ImageBackground style={styles.imageStyle} source={require('../../resources/images/category-pizza.jpg')}>
                         <Text style={styles.title}>
                             {string.categories.pizza}
                         </Text>
                     </ImageBackground>
                     </TouchableOpacity  >
-                    <TouchableOpacity onPress={() => this.props.navigation.navigate('ProductList', {title: string.categories.sidedish, type: 'sidedish'})}>
+                    <TouchableOpacity onPress={() => this.props.navigation.navigate('ProductList', {title: string.categories.sidedish, type: '2'})}>
                     <ImageBackground style={styles.imageStyle} source={require('../../resources/images/category-sidedish.jpg')}>
                         <Text style={styles.title}>
                             {string.categories.sidedish}
                         </Text>
                     </ImageBackground>
                     </TouchableOpacity>
-                    <TouchableOpacity onPress={() => this.props.navigation.navigate('ProductList', {title: string.categories.dessert, type: 'dessert'})}>
+                    <TouchableOpacity onPress={() => this.props.navigation.navigate('ProductList', {title: string.categories.dessert, type: '3'})}>
                     <ImageBackground style={styles.imageStyle} source={require('../../resources/images/category-dessert.jpg')}>
                         <Text style={styles.title}>
                             {string.categories.dessert}
                         </Text>
                     </ImageBackground>
                     </TouchableOpacity >
-                    <TouchableOpacity  onPress={() => this.props.navigation.navigate('ProductList', {title: string.categories.beverage, type: 'beverage'})}>
+                    <TouchableOpacity  onPress={() => this.props.navigation.navigate('ProductList', {title: string.categories.beverage, type: '4'})}>
                     <ImageBackground style={styles.imageStyle} source={require('../../resources/images/category-beverage.jpg')}>
                         <Text style={styles.title}>
                             {string.categories.beverage}

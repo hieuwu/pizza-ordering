42d5cd28e816faf5a137f9b27df64f88f9cac562
hieuvu99 <star.hieu.99@gmail.com>
2020-05-08 07:40:23 +0700

    refactor(product repo): remove switch case
    Remove switch case for each category of products

--- PycozzaProject/app/repositories/ProductRepository/ProductAPI.js
+++ PycozzaProject/app/repositories/ProductRepository/ProductAPI.js
@@ -2,26 +2,10 @@ import BaseAPI from '../../services/BaseAPI'
 
 class ProductAPI {
     async getListProduct(type) {
-        console.log("Get from API");
-        let data = [];
-        switch (type) {
-            case 'pizza': {
-                data = await BaseAPI.get('/product/1');
-                return data.data.products;
-            };
-            case 'sidedish': {
-                data = await BaseAPI.get('/product/2');
-                return data.data.products;
-            };
-            case 'dessert': {
-                data = await BaseAPI.get('/product/3');
-                return data.data.products;
-            };
-            case 'beverage': {
-                data = await BaseAPI.get('/product/4');
-                return data.data.products;
-            };
-        }
+        console.log("Get from API of", type);
+        let url = '/product/'+type;
+        let data = await BaseAPI.get(url);
+        return data.data.products;
     }
 }
 
